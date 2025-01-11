c9de391c10fde0a688f576522bb127cdf2979bd5
Mascot-11 <165539450+Mascot-11@users.noreply.github.com>
2025-01-11 14:49:14 +0545

    removed unneessary comments

--- routes/api.php
+++ routes/api.php
@@ -8,13 +8,15 @@ use App\Notifications\CustomResetPasswordNotification;
 
 
 
-Route::get('/users', [UserAuthController::class, 'users']);
+Route::middleware('auth')->group(function () {
+    Route::get('/users', [UserAuthController::class, 'users']);
+    Route::post('/users', [UserAuthController::class, 'createUser']);
+    Route::put('/users/{id}', [UserAuthController::class, 'updateUser']);
+    Route::delete('/users/{id}', [UserAuthController::class, 'deleteUser']);
+});
+
+// Public routes
 Route::post('register', [UserAuthController::class, 'register'])->name('register');
 Route::post('login', [UserAuthController::class, 'login'])->name('login');
 Route::post('/forgot/password', [UserAuthController::class, 'forgotPassword'])->name('forgotpassword');
 Route::post('password/reset', [UserAuthController::class, 'resetPassword']);
-Route::get('/users', [UserAuthController::class, 'users']);
-Route::post('/users', [UserAuthController::class, 'createUser']);
-Route::put('/users/{id}', [UserAuthController::class, 'updateUser']);
-Route::delete('/users/{id}', [UserAuthController::class, 'deleteUser']);
-
